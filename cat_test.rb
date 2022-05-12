db4b91bc30bf98bebd1381149b9f23fbe7a2c85b
Chelsea Oconnor <Chelseaoconnor27@gmail.com>
2022-05-12 09:55:34 -0500

    130

---
+++ cat_test.rb
@@ -0,0 +1,26 @@
+require 'minitest/autorun'
+require_relative 'cat'
+
+class CatTest < MiniTest::Test
+    def setup
+      @kitty = Cat.new('Kitty',6)
+    end
+    def test_is_not_purrier
+      patch = Cat.new('Patch', 5)
+      milo = Cat.new('Milo', 3)
+    
+      refute(patch.purr_factor > milo.purr_factor)
+    end
+    
+    def test_is_cat; end
+  
+    def test_name
+      assert_equal('Kitty', @kitty.name)
+    end
+  
+    def test_miaow
+      assert_match(/ is miaowing\./, @kitty.miaow)
+    end
+  
+    def test_raises_error; end
+  end
